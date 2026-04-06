b37b7a371991678d863e3e4576a1c3637368ac69
Wilson Vuu <vuuwilson99@gmail.com>
2026-04-06 23:44:15 +0000

    add host_info and host_usage monitoring scripts

---
+++ linux_sql/scripts/host_usage.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+if [ "$#" -ne 5 ]; then
+    echo "Illegal number of parameters"
+    exit 1
+fi
+
+hostname=$(hostname -f)
+vmstat_mb=$(vmstat --unit M)
+
+memory_free=$(echo "$vmstat_mb" | tail -1 | awk '{print $4}' | xargs)
+cpu_idle=$(echo "$vmstat_mb" | tail -1 | awk '{print $15}' | xargs)
+cpu_kernel=$(echo "$vmstat_mb" | tail -1 | awk '{print $14}' | xargs)
+disk_io=$(echo "$vmstat_mb" | tail -1 | awk '{print $9+$10}' | xargs) # Total disk I/O (blocks read + blocks written per second)
+disk_available=$(df -BM / | awk 'NR==2 {gsub("M","",$4); print $4}' | xargs)
+timestamp=$(date -u '+%Y-%m-%d %H:%M:%S')
+
+host_id="(SELECT id FROM host_info WHERE hostname='$hostname')"
+
+insert_stmt="INSERT INTO host_usage(
+    timestamp,
+    host_id,
+    memory_free,
+    cpu_idle,
+    cpu_kernel,
+    disk_io,
+    disk_available
+) VALUES (
+    '$timestamp',
+    $host_id,
+    $memory_free,
+    $cpu_idle,
+    $cpu_kernel,
+    $disk_io,
+    $disk_available
+);"
+
+export PGPASSWORD=$psql_password
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+exit $?
